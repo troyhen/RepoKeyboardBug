57621bc7e8bbb54313ad6d6484e751f49fe05648
Troy <troyhen@churchofjesuschrist.org>
2023-10-23 17:02:37 -0600

    Refactor

--- app/src/main/java/com/repro/keyboardbug/MainActivity.kt
+++ app/src/main/java/com/repro/keyboardbug/MainActivity.kt
@@ -7,18 +7,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.repro.keyboardbug.ui.theme.RepoKeyboardBugTheme
 
@@ -34,26 +29,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var showText by remember { mutableStateOf(false) }
             RepoKeyboardBugTheme {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp), color = MaterialTheme.colorScheme.background
-                ) {
-                    Column {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text("Click to toggle text entry", Modifier.weight(1f), style = MaterialTheme.typography.bodyLarge)
-                            Button({ showText = !showText }) {
-                                Text(if (showText) "Hide" else "Show", style = MaterialTheme.typography.bodyLarge)
-                            }
-                        }
-                        LongList(Modifier.weight(1f))
-                        if (showText) {
-                            DataEntry(Modifier.fillMaxWidth().padding(top = 16.dp))
-                        }
-                    }
+                Scaffold { padding ->
+                    Content(padding)
                 }
+            }
+        }
+    }
+
+    @Composable
+    private fun Content(padding: PaddingValues) {
+        var showText by remember { mutableStateOf(false) }
+        Column(
+            Modifier
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text("Click to toggle text entry", Modifier.weight(1f), style = MaterialTheme.typography.bodyLarge)
+                Button({ showText = !showText }) {
+                    Text(if (showText) "Hide" else "Show", style = MaterialTheme.typography.bodyLarge)
+                }
+            }
+            LongList(Modifier.weight(1f))
+            if (showText) {
+                DataEntry(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                )
             }
         }
     }
@@ -61,7 +65,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun LongList(modifier: Modifier = Modifier) {
-    LazyColumn(modifier.fillMaxWidth().background(MaterialTheme.colorScheme.primaryContainer), contentPadding = PaddingValues(vertical = 8.dp)) {
+    LazyColumn(
+        modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer), contentPadding = PaddingValues(vertical = 8.dp)
+    ) {
         items(100) {
             Text("Item ${it + 1}")
         }
